8e5dc43dd0a483266a97fe374a28e0b559977baa
Linus Nielsen <linus@haxx.se>
2021-12-07 08:27:03 +0100

    Set the CMP0116 policy to be compatible with newer cmake (>=3.20)

--- cmake/app/boilerplate.cmake
+++ cmake/app/boilerplate.cmake
@@ -31,6 +31,13 @@ cmake_minimum_required(VERSION 3.13.1)
 # CMP0002: "Logical target names must be globally unique"
 cmake_policy(SET CMP0002 NEW)
 
+# Use the old CMake behaviour until we are updating the CMake 3.20 as minimum
+# required. This ensure that CMake >=3.20 will be consistent with older CMakes.
+# CMP0116: Ninja generators transform DEPFILE s from add_custom_command().
+if(${CMAKE_VERSION} VERSION_GREATER_EQUAL 3.20)
+  cmake_policy(SET CMP0116 OLD)
+endif()
+
 # Use the old CMake behaviour until the build scripts have been ported
 # to the new behaviour.
 # CMP0079: "target_link_libraries() allows use with targets in other directories"
