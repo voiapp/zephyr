be5d1157768f334aaf907a9c3ab3db1c336cee2f
Martin Schröder <info@swedishembedded.com>
2026-03-05 16:08:17 +0000

    snippets: fix to mkdir to create parents

--- scripts/snippets.py
+++ scripts/snippets.py
@@ -358,7 +358,7 @@ def write_cmake_out(snippets: Snippets, cmake_out: Path) -> None:
     The contents of this file should be considered an implementation
     detail and are not meant to be used outside of snippets.cmake.'''
     if not cmake_out.parent.exists():
-        cmake_out.parent.mkdir()
+        cmake_out.parent.mkdir(parents=True, exist_ok=True)
 
     snippet_data = SnippetToCMakeOutput(snippets).output_cmake()
 
