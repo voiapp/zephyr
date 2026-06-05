fb7efcf29551d955fe2b8b6f05bb17863be2ed2f
jonasberg-voi <jonas.berg@voiapp.io>
2026-06-05 11:41:04 +0200

    stm32 spi: Enable pin control when SPI is idle (#42)
    The MOSI of the STM32H5 SPI controller is floating when the SPI is idle.
    As we use a pull-up on that pin, this causes an idle-high voltage on the MOSI pin.
    
    We use the MOSI pin for control of WS2812 LEDs, and they requires an idle-low voltage.
    
    Add a devicetree setting 'alternate-function-gpio-control' so the SPI controller
    keeps control of the MOSI pin also between transmissions.
    This enables the AFCNTR bit in the STM32 SPI controller, via the LL_SPI_EnableGPIOControl()
    HAL function.
    
    Signed-off-by: Jonas Berg <jonas.berg@voiapp.io>

--- drivers/spi/spi_stm32.c
+++ drivers/spi/spi_stm32.c
@@ -1287,6 +1287,11 @@ static int spi_stm32_configure(const struct device *dev,
 		LL_SPI_EnableIOSwap(cfg->spi);
 	}
 #endif
+	if (cfg->alternate_function_gpio_control) {
+		LL_SPI_EnableGPIOControl(cfg->spi);
+	} else {
+		LL_SPI_DisableGPIOControl(cfg->spi);
+	}
 
 	if (SPI_MODE_GET(config->operation) & SPI_MODE_CPOL) {
 		LL_SPI_SetClockPolarity(spi, LL_SPI_POLARITY_HIGH);
@@ -2173,6 +2178,7 @@ static int spi_stm32_init(const struct device *dev)
 			DT_INST_STRING_UPPER_TOKEN(id, st_spi_data_width)),	\
 		.fifo_enabled = SPI_FIFO_ENABLED(id),				\
 		.ioswp = DT_INST_PROP(id, ioswp),				\
+		.alternate_function_gpio_control = DT_INST_PROP(id, alternate_function_gpio_control), \
 		STM32_SPI_IRQ_HANDLER_FUNC(id)					\
 		IF_ENABLED(DT_HAS_COMPAT_STATUS_OKAY(st_stm32_spi_subghz),	\
 			   (.use_subghzspi_nss =				\
